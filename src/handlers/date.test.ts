7f397fab5a0a3b2ed31ab0139b38ed15ce45bf78
Maurice de Beijer <maurice.de.beijer@gmail.com>
2020-05-02 15:34:50 +0200

    Min/Max on date schemas

--- src/handlers/date.test.ts
+++ src/handlers/date.test.ts
@@ -10,14 +10,38 @@ describe('Create random date value for', () => {
     const schema = yup.date();
     const result = handleDateSchema(schema);
 
-    expect(result).toMatchInlineSnapshot(`1970-01-01T00:00:01.417Z`);
+    expect(result).toBeInstanceOf(Date);
     expect(() => schema.validateSync(result)).not.toThrow();
+    expect(result).toMatchInlineSnapshot(`1834-01-17T06:56:49.374Z`);
   });
 
-  test('to be a Date instance', () => {
-    const schema = yup.date();
+  test('some date after January 1st 2050', () => {
+    const schema = yup.date().min(new Date(2050, 0, 1));
     const result = handleDateSchema(schema);
 
     expect(result).toBeInstanceOf(Date);
+    expect(() => schema.validateSync(result)).not.toThrow();
+    expect(result).toMatchInlineSnapshot(`2446-03-04T06:01:56.730Z`);
+  });
+
+  test('some date before January 1st 1950', () => {
+    const schema = yup.date().max(new Date(1950, 0, 1));
+    const result = handleDateSchema(schema);
+
+    expect(result).toBeInstanceOf(Date);
+    expect(() => schema.validateSync(result)).not.toThrow();
+    expect(result).toMatchInlineSnapshot(`1396-03-03T06:25:50.856Z`);
+  });
+
+  test('some date on May 5th 2020 betweem 15:15 and 15:45', () => {
+    const schema = yup
+      .date()
+      .min(new Date(Date.UTC(2020, 4, 2, 15, 15, 0)))
+      .max(new Date(Date.UTC(2020, 4, 2, 15, 45, 0)));
+    const result = handleDateSchema(schema);
+
+    expect(result).toBeInstanceOf(Date);
+    expect(() => schema.validateSync(result)).not.toThrow();
+    expect(result).toMatchInlineSnapshot(`2020-05-02T15:27:30.640Z`);
   });
 });
--- src/handlers/date.ts
+++ src/handlers/date.ts
@@ -2,5 +2,11 @@ import faker from 'faker';
 import { DateSchema } from 'yup';
 
 export function handleDateSchema(schema: DateSchema): Date {
-  return faker.date.between(new Date(1900), new Date(2100));
+  const minTest = schema.describe().tests.find(t => t.name == 'min');
+  const min = minTest?.params.min ?? new Date(1000, 0, 1);
+
+  const maxTest = schema.describe().tests.find(t => t.name == 'max');
+  const max = maxTest?.params.max ?? new Date(3000, 0, 1);
+
+  return faker.date.between(min, max);
 }
